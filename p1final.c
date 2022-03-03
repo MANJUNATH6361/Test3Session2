1295a30fc87fb546ebf7b3a7b92fc2abf64a78b3
Manjunatha <96220560+MANJUNATH6361@users.noreply.github.com>
2022-03-03 09:43:47 +0000

    p7debug

--- p1final.c
+++ p1final.c
@@ -1,1 +1,18 @@
-#include <stdio.h>
+#include <stdio.h
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  
+}
+
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  
+}
+int main()
+{
+  
+}
--- p8final.c
+++ p8final.c
@@ -1,1 +1,17 @@
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction;
+int find_gcd(int a, int b);
+Fraction input_fraction();
+void input_in_fractions(int n, Fraction f[n]);
+Fraction add_fractions(Fraction f1, Fraction f2);
+Fraction add_n_fractions(int n,Fraction f[n]);
+void output(int n, Fraction f[n], Fraction sum);
 
+int main()
+{
+  Fraction f1,f2,sum;
+  f1=input_fraction();
+}
